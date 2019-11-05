5a22a445d6867016a87e3a3a5d6e139c4895e702
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2019-11-05 15:06:15 +0100

    Add Modular Statix primitives.

--- org.metaborg.spoofax.core/src/main/java/org/metaborg/spoofax/core/SpoofaxModule.java
+++ org.metaborg.spoofax.core/src/main/java/org/metaborg/spoofax/core/SpoofaxModule.java
@@ -221,6 +221,15 @@ import mb.nabl2.terms.stratego.primitives.SG_get_ast_index;
 import mb.nabl2.terms.stratego.primitives.SG_get_ast_resource;
 import mb.nabl2.terms.stratego.primitives.SG_index_ast;
 import mb.nabl2.terms.stratego.primitives.SG_set_ast_index;
+import mb.statix.spoofax.MSTX_end_phase;
+import mb.statix.spoofax.MSTX_end_run;
+import mb.statix.spoofax.MSTX_fix_run;
+import mb.statix.spoofax.MSTX_scope_graph;
+import mb.statix.spoofax.MSTX_solve_constraint;
+import mb.statix.spoofax.MSTX_solve_multi_file;
+import mb.statix.spoofax.MSTX_solve_multi_project;
+import mb.statix.spoofax.MSTX_start_phase;
+import mb.statix.spoofax.MSTX_start_run;
 import mb.statix.spoofax.STX_analysis_has_errors;
 import mb.statix.spoofax.STX_compare_patterns;
 import mb.statix.spoofax.STX_delays_as_errors;
@@ -465,6 +474,15 @@ public class SpoofaxModule extends MetaborgModule {
 
         final Multibinder<AbstractPrimitive> statixLibrary =
             Multibinder.newSetBinder(binder(), AbstractPrimitive.class, Names.named(StatixLibrary.name));
+        bindPrimitive(statixLibrary, MSTX_end_phase.class);
+        bindPrimitive(statixLibrary, MSTX_end_run.class);
+        bindPrimitive(statixLibrary, MSTX_fix_run.class);
+        bindPrimitive(statixLibrary, MSTX_scope_graph.class);
+        bindPrimitive(statixLibrary, MSTX_solve_constraint.class);
+        bindPrimitive(statixLibrary, MSTX_solve_multi_file.class);
+        bindPrimitive(statixLibrary, MSTX_solve_multi_project.class);
+        bindPrimitive(statixLibrary, MSTX_start_phase.class);
+        bindPrimitive(statixLibrary, MSTX_start_run.class);
         bindPrimitive(statixLibrary, STX_analysis_has_errors.class);
         bindPrimitive(statixLibrary, STX_compare_patterns.class);
         bindPrimitive(statixLibrary, STX_delays_as_errors.class);
