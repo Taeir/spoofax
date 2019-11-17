12abdf2af187cc47fb95b1f1d949859ca7695e67
Taico Aerts <devtaeir@taico.nl>
2019-11-17 16:02:57 +0100

    Fix additional extra line

--- org.metaborg.spoofax.core/src/main/java/org/metaborg/spoofax/core/analysis/constraint/AbstractConstraintAnalyzer.java
+++ org.metaborg.spoofax.core/src/main/java/org/metaborg/spoofax/core/analysis/constraint/AbstractConstraintAnalyzer.java
@@ -100,7 +100,6 @@ public abstract class AbstractConstraintAnalyzer implements ISpoofaxAnalyzer {
 
     @Override public ISpoofaxAnalyzeResults analyzeAll(Iterable<ISpoofaxParseUnit> inputs, IContext genericContext,
             IProgress progress, ICancel cancel) throws AnalysisException {
-
         IConstraintContext context;
         try {
             context = (IConstraintContext) genericContext;
